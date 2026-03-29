ec5ba2ef249341fcf7ecbd27d0564dd44d79c995
ambarc1 <ambarclift1@gmail.com>
2026-03-29 17:31:07 -0400

    minor changes and rewrote the ATV page

--- countdown.js
+++ countdown.js
@@ -1,17 +1,13 @@
 var countDownDate = new Date("Oct 17, 2026 01:00:00").getTime();
 
-var x = setInterval(function() //updates every 1s
-{
+var x = setInterval(function() {//updates every 1s
   var now = new Date().getTime();//retrieve dates/time
   var distance = countDownDate - now; //distance between now and the count down date
 
   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-
   document.getElementById("demo").innerHTML = days + " Days ";
 
-  if (distance < 0) 
-  {
+  if (distance < 0) {
     clearInterval(x);
     document.getElementById("demo").innerHTML = "EXPIRED";
   }
