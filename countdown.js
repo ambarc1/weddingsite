5d7e21546ab677925f4b18b9fad5f61107f8eccf
ambarc1 <ambarclift1@gmail.com>
2026-04-06 22:09:31 -0400

    added rsvp form and new coming soon rsvp page

--- countdown.js
+++ countdown.js
@@ -1,15 +1,29 @@
-var countDownDate = new Date("Oct 17, 2026 01:00:00").getTime();
+var countDownDate = new Date("Oct 17, 2026 08:00:00").getTime();
+var countDownDate2 = new Date("Jul 10, 2026 08:00:00").getTime();
 
-var x = setInterval(function() {//updates every 1s
+var x = setInterval(function() //updates every 1s
+{
   var now = new Date().getTime();//retrieve dates/time
   var distance = countDownDate - now; //distance between now and the count down date
+  var distance2 = countDownDate2 - now; //distance between now and the count down date
+
 
   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  document.getElementById("demo").innerHTML = days + " Days ";
 
-  if (distance < 0) {
+  var days2 = Math.floor(distance2 / (1000 * 60 * 60 * 24));
+ 
+  document.getElementById("demo").innerHTML = days + " days ";
+  document.getElementById("demo2").innerHTML = days2 + " days";
+
+  if (distance < 0) 
+  {
     clearInterval(x);
     document.getElementById("demo").innerHTML = "EXPIRED";
   }
-
+  
+  if (distance2 < 0) 
+  {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
 }, 1000);
---
+++ form.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+
+  $('#smartwizard').smartWizard( {
+    selected: 0,
+    theme: 'arrows',
+    autoAdjustHeight:true,
+    transitionEffect:'fade',
+    showStepURLhash: false,
+  });
+});
